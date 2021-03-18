ca53e00a94f4a140e9c6efc9317df3ae014f2632
Bernard Álvares <bernard.luz@gmail.com>
2021-03-18 09:59:34 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,25 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <html lang="en">
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSVTZR8TM8"></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', ${YOUR_TRACKING_ID});`
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </Html>
+      </html>
     )
   }
 }
