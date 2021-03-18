dc14191aaa3dfdb389be2c7d59ab1cbd20945709
Bernard Álvares <bernard.luz@gmail.com>
2021-03-18 14:41:15 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,32 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  render() {
+export default class extends Document {
+  render () {
     return (
-      <Html lang="en">
-        <Head />
+      <html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-PSVTZR8TM8"></script>
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PSVTZR8TM8');
+          `
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </Html>
+      </html>
     )
   }
 }
