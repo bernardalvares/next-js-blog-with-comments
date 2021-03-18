e5c6ccbb551c3d3b89f6fd16533c892d2079aa2c
Bernard Álvares <bernard.luz@gmail.com>
2021-03-18 13:58:09 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,16 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
-        <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSVTZR8TM8"></script>
-          <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-PSVTZR8TM8');
-          </script>
-        </Head>
+      <Html lang="en">
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
+
