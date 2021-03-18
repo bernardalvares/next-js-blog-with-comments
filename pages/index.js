ba43536a7a72c0f4fb0a012f1c1219f0e6dc9d41
Bernard Álvares <bernard.luz@gmail.com>
2021-03-18 14:29:32 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,14 @@ export default function Index({ allPosts, preview }) {
       <Layout preview={preview}>
         <Head>
           <title>Uma página Blog</title>
-          <!-- Global site tag (gtag.js) - Google Analytics -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSVTZR8TM8"></script>
+           <!-- Global site tag (gtag.js) - Google Analytics -->
+           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166954857-1"></script>
           <script>
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
-
-           gtag('config', 'G-PSVTZR8TM8');
-          </script>  
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-166954857-1');
+          </script>
         </Head>
         <Container>
           <Intro />
