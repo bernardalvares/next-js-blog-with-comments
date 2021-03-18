072ac2f4c10e7f2bfc86fc5a12005698001c2dcb
Bernard Álvares <bernard.luz@gmail.com>
2021-03-18 09:45:19 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,16 +4,17 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSVTZR8TM8"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+        <Head>
+         <!-- Global site tag (gtag.js) - Google Analytics -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSVTZR8TM8"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-          gtag('config', 'G-PSVTZR8TM8');
-        </script>
-      <Head />
+            gtag('config', 'G-PSVTZR8TM8');
+         </script>
+        <Head />
         <body>
           <Main />
           <NextScript />
